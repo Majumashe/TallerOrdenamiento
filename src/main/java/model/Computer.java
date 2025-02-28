f02fb1d66e8f36463cb406624c33e2cd692348f5
Majumashe <maria.shek@correounivalle.edu.co>
2025-02-28 08:28:13 -0500

    second commit

--- src/main/java/model/Computer.java
+++ src/main/java/model/Computer.java
@@ -2,13 +2,26 @@ package model;
 
 public class Computer {
     //Atributos
-    //String
+    private String brand;
+    private int ram;
+    private int processors;
+    private double procesingSpeed;
+    private  char operatingSystem;
+
+
     //Relaciones
 
 
     //Constructor
+    private Computer(String brand, int ram, int processors, double procesingSpeed, char operatingSystem){
+        this.brand = brand;
+        this.ram = ram;
+        this.processors = processors;
+        this.operatingSystem = operatingSystem;
 
+    }
 
     //Methods
 
 }
+
